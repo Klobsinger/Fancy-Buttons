463f39b8be7963c14cc62259eddebcc9e49874b3
Kevin Lobsinger <kevlobsinger@gmail.com>
2023-09-07 19:36:00 -0400

    Feat: AngerButton Add setState logic for anger button

--- src/components/AngryButton.js
+++ src/components/AngryButton.js
@@ -1,12 +1,19 @@
 import { useState } from "react";
 
-function AngryButton(props)  {
+function AngryButton(props) {
   const [anger, setAnger] = useState(0);
+
+  const handleClick = (amount) => {
+    if (anger < 1) {
+      setAnger(anger + amount);
+    } else {
+      setAnger(0);
+    }
+  }
+
   return (
-    <button style={{ backgroundColor: `rgba(255,0,0,${anger})` }} className="AngryButton">
-      {/* If you have NOT reached the maximum */}
-      {anger < 1 && <span>Don't click me too much! </span>}
-      {/* If you HAVE reached the maximum */}
+    <button style={{ backgroundColor: `rgba(255,0,0,${anger})` }} onClick={() => handleClick(0.1)} className="AngryButton">
+      {anger < 1 && <span>Don't click me too much!</span>}
       {anger > 1 && <span>Rawr!</span>}
     </button>
   );
